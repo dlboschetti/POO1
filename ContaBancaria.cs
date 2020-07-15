0ca693e4adbc24b6f9e1193ac98d0b1809b5d91b
Diego Boschetti <dlboschetti@gmail.com>
2020-07-14 23:58:37 -0300

    first commit

---
+++ ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace POO1
+{
+    class ContaBancaria
+
+    {
+        private string Conta = "456321-4";
+        private string Agencia = "3030";
+        private string Nome = "DIEGO LUNARDELLI BOSCHETTI";
+        private string Senha = "123456";
+        private int Saldo;
+
+        // Método construtor
+        public ContaBancaria(int saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Depositar (int valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+        }
+
+        public int ConsultarSaldo()
+        {
+            int taxa = 10;
+            int valorTaxa = (Saldo * taxa) / 100;
+            return Saldo - valorTaxa;
+        }
+
+        public void ImprimirSaldos()
+        {
+            Console.WriteLine($"Seu saldo é: {ConsultarSaldo()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria(200);
+
+
+            contaBancaria.Agencia = "34518-3";
+            contaBancaria.Conta = "3030";
+            contaBancaria.Nome = "DIEGO LUNARDELLI BOSCHETTI";
+            contaBancaria.Senha = "senha123";
+
+            contaBancaria.ImprimirSaldos();
+
+            
+
+        }
+    }
+}
